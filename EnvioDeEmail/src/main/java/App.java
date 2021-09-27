9d25897c10e5c4bde53ec50211cddbc9ddbc905d
RafaelRibeiroEstrela <rafael.r.estrela@gmail.com>
2021-09-27 17:34:24 -0400

    Criado

---
+++ EnvioDeEmail/src/main/java/App.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> to = Arrays.asList("brizadeemail@gmail.com");
+		String from = "brizadeemail@gmail.com";
+		String password = "quake3arena";
+
+	}
+
+}
